8fb61a563e00dcf72a4c73f9f85fbda03e59a361
jordanmmartin <jdordan23@gmail.com>
2019-04-24 20:55:04 -0400

    cleaned up card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,4 @@
 class Card < ApplicationRecord
   belongs_to :deck
-  # attr_accessor :rank, :suit, :deck_id
-
-  # def initialize(rank, suit)
-  #   @rank = rank
-  #   @suit = suit
-  # end
 
 end
